321af62ee03df143618049e0f468e91a47e41a2e
Chase Zhang <yun.er.run@gmail.com>
2013-07-25 03:14:54 +0800

    Adding udacity login.

---
+++ emuser/educations/crawler/udacity.py
@@ -0,0 +1,33 @@
+import random
+
+import requests
+
+
+class UdacityClient(object):
+
+    LOGIN_URL = "https://udacity.com/api/session"
+    LOGIN_REFERER_URL = "https://www.udacity.com"
+
+    def __init__(self):
+        self.client = requests.session()
+
+    @property
+    def xsrf_token(self):
+        return self.client.cookies["XSRF-TOKEN"]
+
+    @xsrf_token.setter
+    def xsrf_token(self, value):
+        self.client.cookies["XSRF-TOKEN"] = value
+
+    def login(self, email, password):
+        self.client.get(self.LOGIN_REFERER_URL, verify=False)
+
+        headers = {"Referer": self.LOGIN_REFERER_URL,
+                   "X-Requested-With": "XMLHttpRequest",
+                   "X-XSRFToken": self.xsrf_token}
+        data = {'udacity': {"email": email, "password": password}}
+
+        response = self.client.post(self.LOGIN_URL, data=data, headers=headers,
+                                    verify=False)
+        response.raise_for_status()
+        return response.ok
