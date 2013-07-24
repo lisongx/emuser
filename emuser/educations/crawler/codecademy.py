90b39bd7654e34acb1801504894470f005e2b002
TonySeek <tonyseek@gmail.com>
2013-07-25 03:22:22 +0800

    Finished the login handler of codecademy.

--- emuser/educations/crawler/codecademy.py
+++ emuser/educations/crawler/codecademy.py
@@ -9,6 +9,9 @@ class CodecademyClient(object):
                                 "/input[@name='authenticity_token']"
                                 "/@value")
 
+    LOGIN_REFERER_URL = "http://www.codecademy.com/zh/sign_in"
+    LOGIN_URL = "http://www.codecademy.com/zh/sign_in"
+
     def __init__(self):
         self.client = requests.session()
 
@@ -18,3 +21,14 @@ class CodecademyClient(object):
         tree = htmltree.fromstring(response.text)
         token = tree.xpath(self.BEFORE_LOGIN_TOKEN_XPATH)[0]
         return token
+
+    def login(self, email, password):
+        headers = {"Referer": self.LOGIN_REFERER_URL}
+        data = {"utf8": u'\u2713', "commit": u"\u767b\u9646",
+                "authenticity_token": self.fetch_login_token(),
+                "user[login]": email, "user[password]": password,
+                "user[remember_me]": 1}
+
+        response = self.client.post(self.LOGIN_URL, data=data, headers=headers)
+        response.raise_for_status()
+        return response
