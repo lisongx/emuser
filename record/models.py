a848f37be669c6439cd9c5925b34c5e1239339ff
Chase Zhang <yun.er.run@gmail.com>
2013-07-25 12:10:00 +0800

    添加display选项

--- record/models.py
+++ record/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Record(models.Model):
+    user = models.ForeignKey(User)
+
     source = models.CharField(max_length=32)
     subject = models.CharField(max_length=128)
     url = models.URLField(max_length=128)
     picture_url = models.URLField(max_length=128, null=True)
     datetime =models.DateTimeField()
+    display = models.BoolField(default=True)
+    
