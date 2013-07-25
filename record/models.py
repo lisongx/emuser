7daaaeae73f64b125aafcbe91787af2502d946f4
Chase Zhang <yun.er.run@gmail.com>
2013-07-25 11:30:51 +0800

    添加数据model

---
+++ record/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class Record(models.Model):
+    source = models.CharField(max_length=32)
+    subject = models.CharField(max_length=128)
+    url = models.URLField(max_length=128)
+    picture_url = models.URLField(max_length=128, null=True)
+    datetime =models.DateTimeField()
