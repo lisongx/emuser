82be9d4c87111b5173e9ea328917cde3f62dd15b
Chase Zhang <yun.er.run@gmail.com>
2013-07-25 14:32:35 +0800

    添加profile编辑表单等

--- profile/models.py
+++ profile/models.py
@@ -5,7 +5,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User)
-    bio = models.CharField(max_length=256, blank=True, null=True)
     url = models.URLField(max_length=256, blank=True, null=True)
-    api_token = models.CharField(max_length=60)
-    uid = models.CharField(max_length=60)
+    bio = models.CharField(max_length=256, blank=True, null=True)
